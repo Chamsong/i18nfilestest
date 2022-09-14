3553fe1810b05d2100db5c52d749b1a8bba0f39f
FERNANDEZ GUZMAN <juan-pablo.fernandez-guzman@t-systems.com>
2022-09-14 16:43:09 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import nextI18NextConfig from '../next-i18next.config.js';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default appWithTranslation(MyApp, nextI18NextConfig);
